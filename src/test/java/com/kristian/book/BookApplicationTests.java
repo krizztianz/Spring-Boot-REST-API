767cb842742fa81532a819b76a8695e90893a50a
Kristian Andi <kristianandi33@gmail.com>
2025-06-03 01:57:59 +0700

    Initial Commit

---
+++ src/test/java/com/kristian/book/BookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kristian.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
